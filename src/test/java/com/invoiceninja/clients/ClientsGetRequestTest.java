8eb69f73807e9d87cc0ec394a91d4c2904ce7fb1
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-11-08 15:16:21 +0100

    ClientGetRequestTest impl

---
+++ src/test/java/com/invoiceninja/clients/ClientsGetRequestTest.java
@@ -0,0 +1,56 @@
+package com.invoiceninja.clients;
+
+import com.invoiceninja.BaseTest;
+import io.restassured.http.ContentType;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ClientsGetRequestTest extends AbstractClientsTest {
+
+    @Test
+    public void shouldGetClients() {
+        // @formatter:off
+
+        given()
+                .header(BaseTest.X_TOKEN, getToken())
+        .when()
+                .get(buildRequestUrl(clientsResource))
+        .then()
+                .assertThat()
+                    .statusCode(200)
+                    .contentType(ContentType.JSON);
+
+        // @formatter:on
+    }
+
+    @Test(dataProvider = "data")
+    public void shouldGetClientWithId(int id, String name, String city) {
+        // @formatter:off
+
+        given()
+                .header(BaseTest.X_TOKEN, getToken())
+        .when()
+                .get(buildRequestUrl(clientsResource) + "/" + id)
+        .then()
+                .assertThat()
+                    .statusCode(200)
+                    .contentType(ContentType.JSON)
+                    .body("data.id",  equalTo(id))
+                    .body("data.name", equalTo(name))
+                    .body("data.city", equalTo(city));
+
+        // @formatter:on
+    }
+
+    @DataProvider(name = "data")
+    public Object[][] createTestData() {
+        return new Object[][]{
+                {646, "Test-01", "Kraków"},
+                {645, "Testeer", "Bytom"},
+                {635, "Tester111165985328853412523718875726248384295841499825877723856725532159957573767", "Poznan"}
+        };
+    }
+}
